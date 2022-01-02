5c4e768d2d0d35fb629710f2e6738a33e65fca2a
--replace-all <--replace-all>
2022-01-02 16:22:05 -0500

    agregando rutas en express

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('My App');
+const express = require('express')
+const app = express()
+const morgan  = require('morgan');
+const port = 4000
+
+app.use(morgan('dev'))
+app.get('/',(req,res)=>{
+  res.send('Hola my server Express')
+})
+
+app.get('/newRoute',(req,res)=>{
+  res.send('Hola i am a new route')
+})
+
+app.get('/newJson',(req,res)=>{
+  res.json({
+    name:"product 1",
+    price: 500
+  })
+})
+
+app.listen(port,()=>console.log(`mi port ${port}`))
