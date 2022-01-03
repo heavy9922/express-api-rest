1c48c1bf4ae5b22efd44f5a0274b7f67f94102d5
--replace-all <--replace-all>
2022-01-02 20:09:43 -0500

    se agrego el concepto de servicios al CRUD

--- routes/pruducts.route.js
+++ routes/pruducts.route.js
@@ -1,62 +1,43 @@
 const express = require('express');
-const faker = require('faker');
+const ProductsServices = require('../services/products.services');
 
 const router = express.Router();
+const services = new ProductsServices();
 
-router.get('/', (req, res) => {
-  const { size } = req.query;
-  const limit = size || 5;
-  const products = [];
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      id: i + 1,
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price()),
-      imagen: faker.image.imageUrl(),
-    });
-  }
+router.get('/', async (req, res) => {
+  const products = await services.find();
   res.json(products);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  if(id === '999'){
+  const products = await services.findOne(id);
+  res.json(products);
+});
+
+router.post('/', async (req, res) => {
+  const body = req.body;
+  const newProduct = await services.create(body);
+  res.status(201).json(newProduct);
+});
+
+router.patch('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await services.update(id, body);
+    res.json(product);
+  } catch (e) {
     res.status(404).json({
-      message:"not found"
-    });
-  }else{
-    res.status(200).json({
-      id,
-      name: 'product 1',
-      price: 500,
+      message: e.message,
     });
   }
 });
 
-router.post('/', (req, res) => {
-  const body = req.body;
-  res.status(201).json({
-    message: 'created',
-    data: body,
-  });
-});
-
-router.patch('/:id', (req, res) => {
-  const {id} = req.params;
-  const body = req.body;
-  res.json({
-    message: 'updated',
-    data: body,
-    id
-  });
-});
-
-router.delete('/:id', (req, res) => {
-  const {id} = req.params;
-  res.json({
-    message: 'deleted',
-    id
-  });
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const rta = await services.delete(id);
+  res.json(rta);
 });
 
 module.exports = router;
---
+++ services/products.services.js
@@ -0,0 +1,65 @@
+const faker = require('faker');
+
+class ProductsServices {
+  constructor() {
+    this.products = [];
+    this.generate();
+  }
+
+  generate() {
+    const limit = 100;
+    for (let i = 0; i < limit; i++) {
+      this.products.push({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price()),
+        imagen: faker.image.imageUrl(),
+      });
+    }
+  }
+
+  find() {
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve(this.products)
+      },5000)
+    })
+  }
+
+  async findOne(id) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  async create(data) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      ...data,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  async update(id, changes) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('product not found');
+    }
+    const product = this.products[index]
+    this.products[index] = {
+      ...product,
+      ...changes
+    };
+    return this.products[index];
+  }
+
+  async delete(id) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('product not found');
+    }
+    this.products.splice(index, 1);
+    return { id };
+  }
+}
+
+module.exports = ProductsServices;
