2e600d899fdd5b953a4892a4e1fb12d25c58537f
yefersonCastiblancoNivelics <93614180+yefersonCastiblancoNivelics@users.noreply.github.com>
2022-01-21 15:51:38 -0500

    se dockrizo la app

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler,boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.use(morgan('dev'));
 
 app.use(express.json());
-const whiteList = ['http://127.0.0.1:5500']
-const options = {
-  origin:(origin,callback)=>{
-    if(whiteList.includes(origin)){
-      callback(null,true)
-    }else{
-      callback(new Error('Not allowed'))
-    }
-  }
-}
-app.use(cors(options));
+// const whiteList = ['http://127.0.0.1:5500']
+// const options = {
+//   origin:(origin,callback)=>{
+//     if(whiteList.includes(origin)){
+//       callback(null,true)
+//     }else{
+//       callback(new Error('Not allowed'))
+//     }
+//   }
+// }
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hola my server Express');
